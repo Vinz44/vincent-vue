8c83fb840695695382e30b50650daba7dc1f8ae4
Vinz44 <vinsenthuang044@gmail.com>
2021-11-15 17:46:02 +0700

    Halaman Portofolio (baru)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="hello">
     <div class="container-fluid p-5 bg-primary text-white text-center">
-  <h1>HTML</h1>
+  <h1>Nama : </h1>
   <p>Vincent (2024250011)</p> 
 </div>
 
 <div id="section1" class="container-fluid bg-success text-white" style="padding:100px 20px;">
-  <h1>Apa Itu HTML</h1>
-  <p>HTML adalah bahasa standar pemrogaman yang digunakan untuk membuat halaman website, yang diakses melalui internet. Singkatan dari "Hypertext Markup Language" atau "bahasa markup".</p>
+  <h1>Pendidikan : </h1>
+  <p>TK : KDB 1</p>
+  <p>SD : KDB 1</p>
+  <p>SMP : SMP Indriasana</p>
+  <p>SMK : SMK Xaverius 1 Palembang</p>
+  <p>Universitas : MDP Palembang</p>
 </div>
 
 <div id="section2" class="container-fluid bg-warning" style="padding:100px 20px;">
-  <h1>Fungsi HTML</h1>
-  <p> HTML digunakan untuk membuat dokumen elektronik (disebut halaman) yang ditampilkan di World Wide Web (www). Setiap halaman berisi serangkaian koneksi ke halaman lain yang disebut hyperlink. Setiap halaman web yang Anda lihat di Internet ditulis menggunakan satu versi kode HTML atau yang lain.</p>
-  <p>1. Fungsi utama HTML, untuk membuat suatu halaman website yang bisa dibaca dan dipahami dengan lebih mudah. Seluruh laman website yang ada di internet, dibuat dengan HTML dan tak ada pengecualian.</p>
-  <p>2. Menandai teks pada suatu laman. HTML ditulis pada suatu halaman dokumen dengan tag atau simbol tertentu. Simbol dan tag tersebut akan membuat tampilan teks menjadi tebal, miring, bergaris tebal dan sebagainya. Semisal jika membuat suatu teks menjadi teks miring atau italic, di laman HTML harus ditulis kode    ,   untuk teks tebal dan - - untuk teks bergaris bawah.</p>
+  <h1>Hobby : </h1>
+  <p> Playing Games</p>
+  <p> Drawing Character </p>
+  <p> Swiming </p>
 </div>
 
 <div id="section3" class="container-fluid bg-secondary text-white" style="padding:100px 20px;">
-  <h1>Sejarah HTML</h1>
-  <p>HTML pertama kali dikembangkan oleh Tim Berners-Lee dari organisasi European Organization for Nuclear Research (CERN) tahun 1990. Markup language atau HTML ini diciptakan oleh suatu perusahaan penjual perangkat keras dan lunak yakni IBM atau International Bussiness Machines.</P>
-  <p>Sekitar era 1980an, IBM menciptakan sebuah bahasa yang menggabungkan tag atau simbol dengan teks dalam sebuah dokumen. Bahasa yang diciptakan oleh IBM ini dikenal dengan sebutan markup language atau Generalized markup language (GML).</P>
-  <p>Kemudian rilis versi pertama HTML ditulis oleh Tim Berners-Lee pada 1993. Dikutip dari Washington.edu, sejak itu, mulai banyak versi HTML yang berbeda. Versi yang paling banyak digunakan sepanjang tahun 2000-an adalah HTML 4.01, yang menjadi standar resmi sejak bulan Desember 1999.</P>
+  <h1> Pengalaman :  </h1>
+  <p> Hanya pernah Magang Di Saat masih SMK di Tempat Komputer dan tempat Konsultasi Pajak</P>
+  <p></P>
+  <p></P>
 </div>
 <div class="container mt-3">
-  <h2>Sumber : "https://www.merdeka.com/trending/pengertian-html-lengkap-dengan-fungsi-dan-sejarah-kemunculannya-kln.html"</h2>
+  <h2>Email : vinsenthuang004@gmail.com</h2>
   <p></p> 
   <span class="border border-primary"></span>
   <span class="border border-secondary"></span>
